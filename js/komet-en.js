51442953a4442e03e1c2a086b69a80d9c6293e86
andreaskadhede <andreaskadhede@icloud.com>
2024-04-19 14:08:28 +0200

    done?

---
+++ js/komet-en.js
@@ -0,0 +1,26 @@
+"use strict"
+
+// Når der trykkes på skærmen, føres man videre til cockpittet
+document.addEventListener("click", backToCockpit)
+
+function backToCockpit() {
+    window.location.href = '/cockpit-no-load-en.html';
+};
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let inactivityTime = 12000; // Tiden i millisekunder
+    let timeoutId;
+
+    function timer() {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(redirectToAnotherSite, inactivityTime);
+    }
+
+    function redirectToAnotherSite() {
+        window.location.href = '/cockpit-no-load-en.html'; // Ændrer sidne til cockpit efter 10 sekunder
+    }
+
+    // Starter timeren på ny
+    timer();
+});
